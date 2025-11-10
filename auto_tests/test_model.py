8afbb007c243bf5fe2531724cb873320f2653a22
Dmitrii Prokudin <TheLoyalist@yandex.ru>
2025-11-10 03:02:30 +0300

    First auto-test check

---
+++ auto_tests/test_model.py
@@ -0,0 +1,22 @@
+from src.model import create_model
+from transformers import GPT2TokenizerFast
+
+def test_create_model():
+    tokenizer = GPT2TokenizerFast.from_pretrained('./data/tokenizer/')
+    
+    fake_config = {
+        'model': {
+            'n_positions': 64,
+            'n_embedding': 64,
+            'n_layer': 2,
+            'n_head': 2
+        }
+    }
+
+    model = create_model(fake_config, tokenizer)
+    
+    assert model.config.n_positions == 64
+    assert model.config.n_embedding == 64
+    assert model.config.n_layer == 2
+    assert model.config.n_head == 2
+    assert model.config.vocab_size == tokenizer.vocab_size
